24e046fa3d3b549f4dc8bc99b839d2c45b1ad39c
AyushUnleashed <myzealouslifeofficial@gmail.com>
2023-01-28 05:41:31 +0530

    Adding Input Button & Changed Logo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import profileLogo from '../assets/profile_image.jpg';
+
+import { useState } from 'react';
 
 const Home = () => {
+  const [input,setInput] = useState('');
+
+  const onChange = (event) =>{
+    setInput(event.target.value);
+    console.log({input});
+  };
+
+  const generateAction = ()=>{
+    console.log("Generate btn Works");
+  }
+
   return (
     <div className="root">
       <Head>
@@ -17,19 +31,26 @@ const Home = () => {
             <h2>Turn me into anyone you want! Make sure you refer to me as "aysunpic" in the prompt</h2>
           </div>
           <div className="prompt-container">
-            <input className="prompt-box"/> 
+            <input className="prompt-box" value={input} onChange= {onChange} /> 
+          </div>
+          <div className='prompt-buttons'>
+            <button className='generate-button' onClick={generateAction}>
+              <div className='generate'>
+              <p> Generate </p>
+              </div>
+            </button>
           </div>
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-avatar"
+          href="https://ayushunleashed.carrd.co/"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={profileLogo} alt="profile logo" />
+            <p>Made by Ayush Unleashed</p>
           </div>
         </a>
       </div>
